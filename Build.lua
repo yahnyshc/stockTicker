2c35863b71ca3c53761c32aca19d6ab1b9f51fd2
yahnyshc <yahnyshchak@gmail.com>
2024-07-11 16:01:08 +0100

    Modify Build.lua to use arm architecture when building if compiled on Raspberry Pi

--- Build.lua
+++ Build.lua
@@ -1,8 +1,19 @@
 -- premake5.lua
 workspace "New Project"
-   architecture "x64"
    configurations { "Debug", "Release", "Dist" }
    startproject "App"
+
+   -- Detect architecture based on platform
+   if os.host() == "linux" then
+      local arch = io.popen("uname -m"):read("*l")
+      if arch == "armv7l" or arch == "aarch64" then
+         architecture "ARM"
+      else
+         architecture "x64"  -- Default to x64 for other architectures
+      end
+   else
+      architecture "x64"  -- Default to x64 for non-Linux platforms
+   end
 
    -- Workspace-wide build options for MSVC
    filter "system:windows"
